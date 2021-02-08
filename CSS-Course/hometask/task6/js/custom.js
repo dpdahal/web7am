70e21121e49aa23a4510b9c9fcf138f7c6c395bb
dpdahal <dp48dahal@gmail.com>
2021-02-08 10:48:08 +0545

    update

---
+++ CSS-Course/hometask/task6/js/custom.js
@@ -0,0 +1,3 @@
+// alert('test')
+
+// document.write("Test")
---
+++ javascript/js/custom.js
@@ -0,0 +1,29 @@
+//alert('test');
+
+// window.alert('test');
+
+// document.write("<h1>Hello JavaScript</h1>");
+
+// document.getElementById('test').innerHTML="Hello javascript";
+// document.getElementById('test').style.color = 'red';
+
+// setTimeout(function () {
+//     alert('test');
+// }, 3000);
+
+// setInterval(function () {
+//     alert('test');
+// }, 3000);
+
+// confirm('Are you sure?')
+// prompt("Enter your name")
+
+// document.getElementById('test1')
+//     .innerText="<b>hello</b>"
+//
+//
+// document.getElementById('test2')
+//     .innerHTML="<b>hello</b>"
+
+
+// alert('test')
